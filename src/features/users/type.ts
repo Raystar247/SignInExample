fbfa3418bf3bcdba8e1b7db8ddbddd09b4ad0781
Raystar247 <24star10@gmail.com>
2025-05-29 10:53:46 +0900

    first commit

---
+++ src/features/users/type.ts
@@ -0,0 +1,17 @@
+export type SignInData = {
+    email: string;
+    password: string;
+}
+
+export type User = {
+    id: string;
+    username: string;
+    email: string;
+    password: string;
+}
+
+export type SignInResponse = {
+    isAuthenticated: boolean;
+    id: string;
+}
+
